4900e6b6169490c70c346ee7f0fc3d3fba304b2b
roshaan <RoshaanAli@users.noreply.github.com>
2024-07-11 18:50:26 +0500

    Initial commit

---
+++ src/Components/FooterLinksMapper.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function FooterLinksMapper({ item, mode,  }) {
+  const navigate = useNavigate();
+  let bookName = item?.Title || item.label;
+  return (
+    <a
+      href="#"
+      className="link-item"
+      onClick={(e) => {
+        e.preventDefault();
+        if (mode === "categories") {
+          navigate("/search", { state: { genre: item?.label || item?.Title } });
+        } else if (mode === "sortBy") {
+          navigate(`/book`, {
+            replace: true,
+            state: {
+              book: item,
+            },
+          });
+        }
+      }}
+    >
+      {bookName?.length > 20 ? `${bookName?.substring(0, 27)}...` : bookName}
+    </a>
+  );
+}
+
+export default FooterLinksMapper;
